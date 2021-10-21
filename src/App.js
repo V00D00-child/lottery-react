9c9a97009e384806c659d8d6e1217eba1aa70683
Idris Bowman <corndog2@Idriss-MacBook-Air.local>
2021-10-21 18:14:15 -0400

    add logic to talk to lottery smart contract

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+import web3 from './web3';
+import lottery from "./lottery";
 
 class App extends React.Component {
+  // es2016
+  state = {
+    manager: '',
+    players: [],
+    contractBalance: '',
+    value: '',
+    account: '',
+    message: '',
+    lastWinner: '',
+    currentRound: '',
+    walletEthBalance: ''
+  };
+
+  async componentDidMount() {
+    const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const lastWinner = await lottery.methods.lastWinner().call();
+    const currentRound = await lottery.methods.currentRound().call();
+    const contractBalance = await web3.eth.getBalance(lottery.options.address);
+    const walletEthBalance = await web3.eth.getBalance(accounts[0]);
+
+    const formatedPlayers = players.map(player => {
+      return player.toLowerCase()
+    });
+
+    this.setState({ 
+      manager: manager.toLowerCase(),
+      players:formatedPlayers,
+      contractBalance,
+      account: accounts[0].toLowerCase(),
+      lastWinner: lastWinner.toLowerCase(),
+      currentRound,
+      walletEthBalance
+    });
+
+    lottery.events.Enter({}, (error, event) => {
+      console.log(event);
+      console.log(event.returnValues['0']);
+      const players = event.returnValues['0'];
+      const contractBalance = event.returnValues['1'];
+
+      const formatedPlayers = players.map(player => {
+        return player.toLowerCase()
+      });
+
+      this.setState({ 
+        players:formatedPlayers,
+        contractBalance,
+      });
+    });
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+    this.setState({ message: 'Waiting on transaction success...' });
+
+    await lottery.methods.enter().send({
+      from: this.state.account,
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+
+    this.setState({ message: 'You have been entered!' });
+  }
+
+  pickWinner = async (event) => {
+    event.preventDefault();
+
+    this.setState({ message: 'Waiting on transaction success...' });
+
+    await lottery.methods.pickWinner().send({
+      from: this.state.account,
+    });
+
+    const lastWinner = await lottery.methods.lastWinner().call();
+
+    this.setState({ message: `A winner has been picked ${lastWinner}`, lastWinner: lastWinner.toLowerCase() });
+  }
+
   render() {
+    const { manager, players, contractBalance, value, account, message, lastWinner, currentRound, walletEthBalance } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="">
+        <h2>Lottery contract{account === manager && <span>(manager)</span>}</h2>
+        <p>My account: {account}</p>
+        <p>My wallet balance: {web3.utils.fromWei(walletEthBalance, 'ether')} ETH</p>
+        <hr></hr>
+        <p>Current lottery round: {currentRound}</p>
+        <p>Last winner: {lastWinner}</p>
+        <p>This contract is managed by: {manager}.
+        There are currently {players.length} people entered,
+        competing to win {web3.utils.fromWei(contractBalance, 'ether')} Ether!
+        </p>
+
+        <hr></hr>
+
+      {/* Enter into lottery */}
+      {
+        players.indexOf(account) === -1 && account !== manager ? 
+          <form onSubmit={this.onSubmit}>  
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              value={value}
+              onChange={event => this.setState({ value: event.target.value, message: '' })}
+            />
+          </div>
+          <button>Enter</button>
+          </form>
+        :
+        <p>You cannot enter into lottery!</p>
+      }
+      
+        <hr></hr>
+
+        {/* Manger pick a winner */}
+        {
+          account === manager &&
+          <div>
+            <h4>Ready to pick a winner?</h4>
+            <button onClick={this.pickWinner}>Pick winner</button>
+          </div>
+        }
+        <hr></hr>
+        <p>{message}</p>
       </div>
     );
   }
---
+++ src/lottery.js
@@ -0,0 +1,99 @@
+import web3 from "./web3";
+
+const address = "0x57E6C6A88392b4e4B228bC0A5ed36dC054ffa543";
+
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [
+      { name: "", type: "address" },
+      { name: "", type: "uint256" },
+    ],
+    name: "activeInLottery",
+    outputs: [{ name: "", type: "bool" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "currentRound",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "lastWinner",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: false, name: "players", type: "address[]" },
+      { indexed: false, name: "balance", type: "uint256" },
+      { indexed: false, name: "timestamp", type: "uint256" },
+    ],
+    name: "Enter",
+    type: "event",
+  },
+];
+
+export default new web3.eth.Contract(abi, address);
